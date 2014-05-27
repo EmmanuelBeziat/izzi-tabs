b4ef49b968b6ab0d87ecb28b5bfaa30f55cff557
Emmanuel B <contact@emmanuelbeziat.com>
2014-05-27 04:59:44 +0200

    Propreté des variables

--- jquery.tabs.js
+++ jquery.tabs.js
@@ -1,8 +1,9 @@
 /**
-* Système d'onglets automatisé
-* Version : 2.1
-* Par Emmanuel "Manumanu" B. (www.emmanuelbeziat.com)
-**/
+ * Système d'onglets automatisé
+ * Version : 2.1
+ * Par Emmanuel "Manumanu" B. (www.emmanuelbeziat.com)
+ * https://github.com/RhooManu/jQuery-Tabs
+ **/
 
 (function($) {
 	$.fn.tabs = function(params) {
@@ -18,12 +19,12 @@
 		this.each(function() {
 
 			// Variables
-			var page = $('html, body');
-			var tabContainer = $(this);
-			var tabFirst = tabContainer.find('li:first a');
-			var tabCurrent = null;
-			var tabID = null;
-			var tabAnchor = window.location.hash;
+			var page = $('html, body'),
+				tabContainer = $(this),
+				tabFirst = tabContainer.find('li:first a'),
+				tabCurrent = null,
+				tabID = null,
+				tabAnchor = window.location.hash;
 
 			// Attribuer l'onglet par défaut comme étant le premier, ou utiliser l'ancre
 			if (params.anchors == true && tabAnchor != '' && tabContainer.find('a[data-toggle="tab"]'))
@@ -40,10 +41,10 @@
 			tabContainer.find('a[data-toggle="tab"]').on('click', function(event) {
 
 				tabID = $(this).attr('href');
-				
+
 				// Si l'élément n'est pas déjà sélectionné
 				if (tabID != tabCurrent) {
-					
+
 					// Afficher le contenu demandé et masquer le contenu restant
 					switch (params.mode) {
 						case ('fade'):
@@ -57,15 +58,17 @@
 							$(tabID).fadeIn(params.duration).siblings().hide();
 							break;
 					}
-		
+
 					// Retirer la classe des autres onglets et l'ajouter sur l'élément sélectionné
 					$(this).addClass(params.class).parent('li').siblings().children('a').removeClass(params.class);
 					tabCurrent = tabID;
 				}
 
 				// Empêche le déclenchement du lien si voulu
-				if (params.anchors == true)
-					setTimeout(function() { page.scrollTop(0, 0);}, 1);
+				if (params.anchors)
+					setTimeout(function() {
+						page.scrollTop(0, 0);
+					}, 1);
 				else
 					event.preventDefault();
 			});
